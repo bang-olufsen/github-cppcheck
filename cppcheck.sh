bdf450bb183b19625f99a72095af5fbf479c58c7
Jeppe Frandsen <jfd@algrens.dk>
2016-08-16 16:41:27 -0700

    Added check for pull request to avoid calling Github API

--- cppcheck.sh
+++ cppcheck.sh
@@ -8,13 +8,15 @@ BRANCH=${BRANCH:=develop}
 CPPCHECK_ARGS=${CPPCHECK_ARGS:="--enable=warning --suppressions-list=cppcheck.txt --template='[{file}:{line}]:({severity}),{id},{message}' --force -q -j `nproc`"}
 
 status () {
-  if [ "$SHIPPABLE" = true ]; then
+  if [ "$SHIPPABLE" = "true" ]; then
     # Limit the description to 100 characters even though GitHub supports up to 140 characters
-    DESCRIPTION=`echo $2 | cut -b -100`
-    DATA="{ \"state\": \"$1\", \"target_url\": \"$BUILD_URL\", \"description\": \"$DESCRIPTION\", \"context\": \"cppcheck\"}"
-    GITHUB_API="https://api.github.com/repos/$REPO_FULL_NAME/statuses/$COMMIT"    
-    curl -H "Content-Type: application/json" -H "Authorization: token $CPPCHECK_TOKEN" -H "User-Agent: bangolufsen/cppcheck" -X POST -d "$DATA" $GITHUB_API 1>/dev/null 2>&1
-    
+    if [ "$IS_PULL_REQUEST" = "true" ]; then
+      DESCRIPTION=`echo $2 | cut -b -100`
+      DATA="{ \"state\": \"$1\", \"target_url\": \"$BUILD_URL\", \"description\": \"$DESCRIPTION\", \"context\": \"cppcheck\"}"
+      GITHUB_API="https://api.github.com/repos/$REPO_FULL_NAME/statuses/$COMMIT"
+      curl -H "Content-Type: application/json" -H "Authorization: token $CPPCHECK_TOKEN" -H "User-Agent: bangolufsen/cppcheck" -X POST -d "$DATA" $GITHUB_API 1>/dev/null 2>&1
+    fi
+
     # Only update coverage badge if we are analyzing all files
     if [ "$FILES" = "." ] && [ "$1" != "pending" ]; then
       BADGE_COLOR=red
@@ -24,13 +26,13 @@ status () {
           BADGE_COLOR=brightgreen
         fi
       fi
-      
+
       BADGE_TEXT=$BUGS"_bug"`test $BUGS -eq 1 || echo s`
       wget -O /tmp/cppcheck_${REPO_NAME}_${BRANCH}.svg https://img.shields.io/badge/cppcheck-$BADGE_TEXT-$BADGE_COLOR.svg 1>/dev/null 2>&1
       curl -H "Authorization: Bearer $DROPBOX_TOKEN" https://api-content.dropbox.com/1/files_put/auto/ -T /tmp/cppcheck_${REPO_NAME}_${BRANCH}.svg 1>/dev/null 2>&1
     fi
   fi
-  
+
   echo $2
 }
 
