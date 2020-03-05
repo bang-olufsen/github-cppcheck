c8182731b27e3c3f4f2f0351dc010cf9062ed151
jeppefrandsen <jeppe@algrens.dk>
2020-03-05 19:25:42 +0100

    Updated to support GitHub Actions

--- README.md
+++ README.md
@@ -2,15 +2,11 @@
 
 [![License](https://img.shields.io/badge/license-MIT_License-blue.svg?style=flat)](LICENSE)
 
-Shell script for running the [Cppcheck](http://cppcheck.sourceforge.net) static code analysis tool for C/C++ source files. The script can either be executed from your build script or as part of GitHub pull requests with status updates (pending, success and failure). This makes it possible to add cppcheck as a required CI step on GitHub. Currently only [Shippable](http://www.shippable.com) CI is supported for knowing the repository, branch and linking to build logs.
-
-![](https://raw.githubusercontent.com/bang-olufsen/github-cppcheck/master/images/cppcheck_github.png)
+Shell script for running the [Cppcheck](http://cppcheck.sourceforge.net) static code analysis tool for C/C++ source files. The script can either be executed from your build script or as part of GitHub pull requests with status updates (pending, success and failure). This makes it possible to add cppcheck as a required CI step on GitHub. Currently only [Shippable](http://www.shippable.com) CI and GitHub Actions is supported for knowing the repository, branch and linking to build logs.
 
 Input can either be a directory (e.g. ".") or specific files. For pull requests the special "diff" command can be parsed as argument to check only the changed files.
 
 When all files are checked (e.g. as part of nightly builds) the Cppcheck badge is updated for the particular repository and branch. The badge is afterwards uploaded to [Dropbox](http://www.dropbox.com). For displaying the badge on your README.md copy the Dropbox link to the image and replace www.dropbox.com with dl.dropboxusercontent.com. The color of the badge will be red in case of errors, yellow in case of only warnings and green if no bugs have been found.
-
-![](https://raw.githubusercontent.com/bang-olufsen/github-cppcheck/master/images/cppcheck_green.png) ![](https://raw.githubusercontent.com/bang-olufsen/github-cppcheck/master/images/cppcheck_yellow.png) ![](https://raw.githubusercontent.com/bang-olufsen/github-cppcheck/master/images/cppcheck_red.png)
 
 False positives can be suppressed using the cppcheck.txt file which must be located in the directory from where the script is executed. You can run the shell script directly from GitHub using:
 
---
+++ github-cppcheck.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -e
+
+# GITHUB_TOKEN is a GitHub private access token configured for repo:status scope
+# DROPBOX_TOKEN is an access token for the Dropbox API
+
+BRANCH=${BRANCH:=master}
+CPPCHECK_ARGS=${CPPCHECK_ARGS:="--enable=warning --suppressions-list=cppcheck.txt --template='[{file}:{line}]:({severity}),{id},{message}' --force -q -j $(nproc)"}
+
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+  REPO_NAME=$(basename "$GITHUB_REPOSITORY")
+  REPO_FULL_NAME="$GITHUB_REPOSITORY"
+  PULL_REQUEST=$(echo "$GITHUB_REF" | cut -d '/' -f3)
+fi
+
+status () {
+  if [ "$SHIPPABLE" = "true" ] || [ "$GITHUB_ACTIONS" = "true" ]; then
+    if [ "$PULL_REQUEST" != "" ]; then
+      DESCRIPTION=$(echo "$2" | cut -b -100)
+      DATA="{ \"state\": \"$1\", \"description\": \"$DESCRIPTION\", \"context\": \"github / cppcheck\"}"
+      PULL_REQUEST_STATUS=$(curl -s -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" -H "User-Agent: $REPO_FULL_NAME" -X GET "https://api.github.com/repos/$REPO_FULL_NAME/pulls/$PULL_REQUEST")
+      STATUSES_URL=$(echo "$PULL_REQUEST_STATUS" | jq -r '.statuses_url')
+      curl -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" -H "User-Agent: $REPO_FULL_NAME" -X POST -d "$DATA" "$STATUSES_URL" 1>/dev/null
+    fi
+
+    if [ "$FILES" = "." ] && [ "$1" != "pending" ]; then
+      BADGE_COLOR=red
+      if [ "$ERRORS" -eq 0 ]; then
+        BADGE_COLOR=yellow
+        if [ "$WARNINGS" -eq 0 ]; then
+          BADGE_COLOR=brightgreen
+        fi
+      fi
+
+      BADGE_TEXT=$BUGS"_bug"$(test "$BUGS" -eq 1 || echo s)
+      wget -O /tmp/cppcheck_"${REPO_NAME}"_"${BRANCH}".svg https://img.shields.io/badge/cppcheck-"$BADGE_TEXT"-"$BADGE_COLOR".svg 1>/dev/null 2>&1
+      curl -X POST "https://api-content.dropbox.com/2/files/upload" \
+        -H "Authorization: Bearer $DROPBOX_TOKEN" \
+        -H "Content-Type: application/octet-stream" \
+        -H "Dropbox-API-Arg: {\"path\": \"/cppcheck_${REPO_NAME}_${BRANCH}.svg\", \"mode\": \"overwrite\"}" \
+        --data-binary @/tmp/cppcheck_"${REPO_NAME}"_"${BRANCH}".svg 1>/dev/null 2>&1
+    fi
+  fi
+
+  echo "$2"
+}
+
+ARGS=("$@")
+FILES=${ARGS[${#ARGS[@]}-1]}
+unset "ARGS[${#ARGS[@]}-1]"
+
+if [ "$FILES" = "diff" ]; then
+  FILES=$(git diff --name-only --diff-filter ACMRTUXB $BRANCH | grep -e '\.h$' -e '\hpp$' -e '\.c$' -e '\.cc$' -e '\cpp$' -e '\.cxx$' | xargs)
+fi
+
+status "pending" "Running cppcheck with args $CPPCHECK_ARGS ${ARGS[*]} $FILES"
+cppcheck $CPPCHECK_ARGS "${ARGS[*]}" "$FILES" 2>&1 | tee /tmp/cppcheck.log
+
+ERRORS=$(grep -c "(error)" /tmp/cppcheck.log || true)
+WARNINGS=$(grep -c "(warning)" /tmp/cppcheck.log || true)
+BUGS=$((ERRORS + WARNINGS))
+DESCRIPTION="Found $ERRORS error$(test "$ERRORS" -eq 1 || echo s) and $WARNINGS warning$(test "$WARNINGS" -eq 1 || echo s)"
+
+if [ "$ERRORS" -eq 0 ] && [ "$WARNINGS" -eq 0 ]; then
+  status "success" "$DESCRIPTION"
+else
+  status "failure" "$DESCRIPTION"
+fi
